53b585526825f662e56aa4e5dc70e1367516e526
Guilherme Frizzera Meira <guifrizzera2005@gmail.com>
2019-10-19 18:58:40 -0300

    Configurando o background das telas SignIn e SignUp

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Text } from 'react-native';
 
+import Background from '~/components/Background';
+
 import { Container } from './styles';
 
 export default function SignIn() {
   return (
-    <Container>
-      <Text>SignIn</Text>
-    </Container>
+    <Background>
+      <Container>
+        <Text>SignIn</Text>
+      </Container>
+    </Background>
   );
 }
