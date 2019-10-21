424892c8667818781ac91d48955bb510305db1ce
Guilherme Frizzera Meira <guifrizzera2005@gmail.com>
2019-10-21 09:54:54 -0300

    Configuração da StatusBar de forma global para aplicação

--- src/index.js
+++ src/index.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import { Platform, StatusBar } from 'react-native';
 
 import '~/config/ReactotronConfig';
 
 import Routes from '~/routes';
 
 export default function App() {
-  return <Routes />;
+  function defineStatusBar() {
+    if (Platform.OS === 'android' && Platform.Version > 22) {
+      return (
+        <StatusBar
+          translucent
+          barStyle="dark-content"
+          backgroundColor="transparent"
+        />
+      );
+    }
+
+    return <StatusBar translucent backgroundColor="rgba(0, 0, 0, 0.1)" />;
+  }
+
+  return (
+    <>
+      {defineStatusBar()}
+      <Routes />
+    </>
+  );
 }
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Image, Platform, StatusBar } from 'react-native';
+import { Image } from 'react-native';
 
 import logo from '~/assets/logo.png';
 import Background from '~/components/Background';
@@ -15,23 +15,8 @@ import {
 } from './styles';
 
 export default function SignIn({ navigation }) {
-  function defineStatusBar() {
-    if (Platform.OS === 'android' && Platform.Version > 22) {
-      return (
-        <StatusBar
-          translucent
-          barStyle="dark-content"
-          backgroundColor="transparent"
-        />
-      );
-    }
-
-    return <StatusBar translucent backgroundColor="rgba(0, 0, 0, 0.1)" />;
-  }
-
   return (
     <Background>
-      {defineStatusBar()}
       <ScrollingContainer>
         <Container>
           <Image source={logo} />
