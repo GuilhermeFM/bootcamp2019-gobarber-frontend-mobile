b4fd9fdf2e9916fd7eafb6fac1d1ab719574b279
Guilherme Frizzera Meira <guifrizzera2005@gmail.com>
2019-10-23 10:00:44 -0300

    Conectando o redux ao SignIn

--- src/index.js
+++ src/index.js
@@ -1,23 +1,29 @@
 import React from 'react';
 import { Platform, StatusBar } from 'react-native';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from 'react-redux';
 
 import '~/config/ReactotronConfig';
+
+import { store, persistor } from './store';
 
 import Routes from '~/routes';
 
 export default function App() {
   return (
-    <>
-      {Platform.OS === 'android' && Platform.Version > 22 ? (
-        <StatusBar
-          translucent
-          barStyle="dark-content"
-          backgroundColor="transparent"
-        />
-      ) : (
-        <StatusBar translucent backgroundColor="rgba(0, 0, 0, 0.1)" />
-      )}
-      <Routes />
-    </>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        {Platform.OS === 'android' && Platform.Version > 22 ? (
+          <StatusBar
+            translucent
+            barStyle="dark-content"
+            backgroundColor="transparent"
+          />
+        ) : (
+          <StatusBar translucent backgroundColor="rgba(0, 0, 0, 0.1)" />
+        )}
+        <Routes />
+      </PersistGate>
+    </Provider>
   );
 }
--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,10 +1,12 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import { Image, Keyboard } from 'react-native';
+import { useDispatch } from 'react-redux';
 import PropTypes from 'prop-types';
 
 import logo from '~/assets/logo.png';
 
 import Background from '~/components/Background';
+import { singInRequest } from '~/store/modules/auth/actions';
 
 import {
   ScrollingContainer,
@@ -17,8 +19,12 @@ import {
 } from './styles';
 
 export default function SignIn({ navigation }) {
+  const dispatch = useDispatch();
   const passwordRef = useRef();
   const refScrollingContainer = useRef();
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
   useEffect(() => {
     const keyboardDidShowListenner = Keyboard.addListener(
@@ -32,7 +38,9 @@ export default function SignIn({ navigation }) {
     };
   }, []);
 
-  function handleSubmit() {}
+  function handleSubmit() {
+    dispatch(singInRequest(email, password));
+  }
 
   return (
     <Background>
@@ -49,6 +57,8 @@ export default function SignIn({ navigation }) {
               blurOnSubmit={false}
               returnKeyType="next"
               onSubmitEditing={() => passwordRef.current.focus()}
+              value={email}
+              onChangeText={setEmail}
             />
 
             <FormInput
@@ -57,6 +67,8 @@ export default function SignIn({ navigation }) {
               placeholder="Digite sua senha"
               ref={passwordRef}
               returnKeyType="send"
+              value={password}
+              onChangeText={setPassword}
             />
             <SubmitButton onPress={handleSubmit}>Acessar</SubmitButton>
           </Form>
